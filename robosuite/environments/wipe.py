4970494d9016859fd0d14a5f401d75dfbbb4e00b
charleslin4 <charleslin4@gmail.com>
2020-11-23 23:28:36 -0800

    increase num pegs

--- robosuite/environments/wipe.py
+++ robosuite/environments/wipe.py
@@ -30,7 +30,7 @@ DEFAULT_WIPE_CONFIG = {
     "line_width": 0.04,                             # Width of the line to wipe (diameter of the pegs)
     "two_clusters": False,                           # if the dirt to wipe is one continuous line or two
     "coverage_factor": 0.6,                         # how much of the table surface we cover
-    "num_sensors": 20,                               # How many particles of dirt to generate in the environment
+    "num_sensors": 50,                               # How many particles of dirt to generate in the environment
 
     # settings for thresholds
     "contact_threshold": 3,                         # Minimum eef force to qualify as contact [N]
